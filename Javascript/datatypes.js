38f678e7785edb7b5f2419e83ee8252cd537875a
Kashaf Fatima <kashaffatimah10@gmail.com>
2025-06-13 06:11:55 +0000

    commit

---
+++ Javascript/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS as newer version of javascript
+//alert(3+3) //we are using node js,not browser, so donot use alert here
+
+console.log(3+3)
+console.log("kashaf")
+
+let name = "kashaf" //string datatype
+let age = 20    // number datatype
+let isLoggedIn = true
+let state = null
+let notAssigned;  //undefined
+
+
+
+//number => 2 to power of 53
+//bigint
+//string => ""
+//boolean => true or false
+//null => empty value (standalone value) type of null is object
+//undefined => variable is declared but not assigned any value
+//symbol => unique value
+
+//object
+
+console.log(typeof name) //string
+console.log(typeof age)  //number
+console.log(typeof isLoggedIn) //boolean
+console.log(typeof state)  //type of null is object
+console.log(typeof notAssigned)  //undefined
