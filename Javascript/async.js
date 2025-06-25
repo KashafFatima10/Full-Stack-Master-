79756555bc44dc074eb77b558d562dcf5f2c0c65
Kashaf Fatima <kashaffatimah10@gmail.com>
2025-06-25 07:51:21 +0000

    Asyncs Javascript

---
+++ Javascript/async.js
@@ -0,0 +1,13 @@
+/****javascript*****(by-default)
+ =>synchronus
+ =>single threaded*/
+
+/***EXECUTION CONTEXT***
+  single line  of code excute at a time
+  every operation waits for last operation to complete before executing
+*/
+
+/* Blocking code=>(read file sync)(block flow of program)
+                        vs
+ non blocking code=>(read file async)(does not block execution)
+ */
